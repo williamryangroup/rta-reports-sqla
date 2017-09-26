78deba71d12d84e1368974672a491682bc478efa
williambrian <brian@williambrian.com>
2017-09-26 13:47:29 -0400

    Update location insert for SYNKROS
    Use locations from EVENT table when inserting locations for SYNCKROS feedtype

--- SQL/sp_SQLA_Insert_Locations.sql
+++ SQL/sp_SQLA_Insert_Locations.sql
@@ -31,14 +31,34 @@ BEGIN
 
 	DECLARE @UseAssetField int = isnull((select case when Setting = 'Asset' then 1 else 0 end from RTSS.dbo.SYSTEMSETTINGS where ConfigSection = 'RTSSHH' and ConfigParam = 'EventLocationOrAssetFieldName'),0)
 	DECLARE @UseArea int = isnull((select case when ltrim(rtrim(Setting)) = '1' then 1 else 0 end from RTSS.dbo.SYSTEMSETTINGS where ConfigSection = 'SYSTEM' and ConfigParam = 'AssociatedAreasMode'),0)
-	
+	DECLARE @FeedType varchar(50) = (select ltrim(rtrim(Setting)) from RTSS.dbo.SYSTEMSETTINGS where ConfigSection = 'SYSTEM' and ConfigParam = 'FeedType')
+
 	truncate table SQLA_Locations
 	
-	insert into SQLA_Locations (Location, Asset, Zone, Area, IsActive, DisplayLocation, ZoneArea)
-	select Location, Asset, Zone, Area, IsActive,
-	       DisplayLocation = case when @UseAssetField = 1 then Asset else Location end,
-	       ZoneArea = case when @UseArea = 1 then Area else Zone end
-	  from RTSS.dbo.LOCZONE WITH (NOLOCK)
+	IF (@FeedType <> 'SYNKROS')
+	BEGIN
+		insert into SQLA_Locations (Location, Asset, Zone, Area, IsActive, DisplayLocation, ZoneArea)
+		select Location, Asset, Zone, Area, IsActive,
+			   DisplayLocation = case when @UseAssetField = 1 then Asset else Location end,
+			   ZoneArea = case when @UseArea = 1 then Area else Zone end
+		  from RTSS.dbo.LOCZONE WITH (NOLOCK)
+	END
+
+
+	IF (@FeedType = 'SYNKROS')
+	BEGIN
+		insert into SQLA_Locations (Location, Asset, Zone, Area, IsActive, DisplayLocation, ZoneArea)
+		select distinct Location, Asset, Zone, Area = Zone, IsActive = 1,
+			   DisplayLocation = case when @UseAssetField = 1 then Asset else Location end,
+			   ZoneArea = Zone
+		  from RTSS.dbo.EVENT4 as e1 WITH (NOLOCK)
+		 where Location like '_%-___-_%'
+		   and exists 
+		     ( select null from RTSS.dbo.EVENT4 as e2 WITH (NOLOCK)
+			    where e2.Location = e1.Location
+				group by e2.Location
+				having max(e2.tout) = e1.tOut )
+	END
 	
 END
 
