c636da4399303ae8e41074388d47bc228c61b83d
williambrian <brian@williambrian.com>
2017-09-26 14:04:38 -0400

    Meal book updates for DROP TEAM
    1- Updates for DROP TEAM to not display duplicate card number
    2- Updates to display proper EmpName for DROP TEAM events

--- SQL/sp_SSRS_Rpt_RTA_MEAL.sql
+++ SQL/sp_SSRS_Rpt_RTA_MEAL.sql
@@ -32,18 +32,25 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 	
-	SELECT PktNum, Location, Zone, EmpNum, EmpName = isnull(m.EmpName,emp.NameFirst+' '+left(emp.NameLast,1)+'.'), EmpLicNum, tOut, EntryReason, ParentEventID,
+	SELECT PktNum, Location, Zone, EmpNum,
+		   EmpName = case when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is not null then emp.NameFirst+' '+left(emp.NameLast,1)+'.'
+		                  when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is null and emp2.CardNum is not null then emp2.NameFirst+' '+left(emp2.NameLast,1)+'.'
+						  else m.EmpName end,
+		   EmpLicNum, tOut, EntryReason, ParentEventID,
 		   PktNumWitness1, PktNumSourceWitness1, EmpNumWitness1, EmpNameWitness1, EmpLicNumWitness1, tWitness1,
 		   PktNumWitness2, PktNumSourceWitness2, EmpNumWitness2, EmpNameWitness2, EmpLicNumWitness2, tWitness2,
 		   PktNumWitness3, PktNumSourceWitness3, EmpNumWitness3, EmpNameWitness3, EmpLicNumWitness3, tWitness3,
 		   PktNumWitness4, PktNumSourceWitness4, EmpNumWitness4, EmpNameWitness4, EmpLicNumWitness4, tWitness4,
-		   EventComment, Asset, EventDescription = isnull(r.EventDescription,m.CardInEvtDesc)
+		   EventComment, Asset, 
+		   EventDescription = case when m.EntryReason = 'DROP TEAM' then m.CardInEvtDisp else isnull(r.EventDescription,m.CardInEvtDesc) end
 	  FROM SQLA_MEAL as m
 	  left join SQLA_CardInReasons as r
 	    on r.Dept = m.Source
 	   and r.EventDisplay = m.CardInEvtDisp
 	  left join SQLA_Employees as emp
 	    on emp.CardNum = m.EmpNum
+	  left join SQLA_Employees as emp2
+	    on emp2.CardNum = ltrim(rtrim(m.CardInEvtDisp))
 	 WHERE tOut >= @StartDt and tOut <= @EndDt
 	   and ((@Location = '') or (@Location = ' All') or (Location = @Location) or (Asset = @Location))
 END
