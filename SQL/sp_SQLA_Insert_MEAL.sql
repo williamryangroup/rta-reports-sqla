aa0f1eb2c85cf53b72e81c1f32719ec2c82e3aa5
williambrian <brian@williambrian.com>
2019-03-14 15:30:20 -0400

    Updated MEAL book - Removed Completion Code from displaying in EventComment

--- SQL/sp_SQLA_Insert_MEAL.sql
+++ SQL/sp_SQLA_Insert_MEAL.sql
@@ -8,13 +8,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-IF EXISTS (select * from dbo.sysobjects where id = object_id(N'[dbo].[sp_SQLA_Insert_MEAL]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+IF EXISTS (select *
+	         from dbo.sysobjects
+	        where id = object_id(N'[dbo].[sp_SQLA_Insert_MEAL]')
+			  and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 	DROP PROCEDURE [sp_SQLA_Insert_MEAL]
 GO
-
-
 
 -- =============================================
 -- Author:		<Author,,Name>
@@ -22,25 +21,41 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_SQLA_Insert_MEAL]
-	
+
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	
-	DECLARE @UseAssetField char(255) = isnull((select case when Setting = 'Asset' then '1' else '0' end from RTSS.dbo.SYSTEMSETTINGS WITH (NOLOCK) where ConfigSection = 'RTSSHH' and ConfigParam = 'EventLocationOrAssetFieldName'),'0')
-	
-	insert into SQLA_MEAL (PktNum, Location, Zone, EmpNum, EmpName, EmpLicNum, tOut, EntryReason, ParentEventID, PktNumWitness1, PktNumSourceWitness1, EmpNumWitness1, EmpNameWitness1, EmpLicNumWitness1, tWitness1, PktNumWitness2, PktNumSourceWitness2, EmpNumWitness2, EmpNameWitness2, EmpLicNumWitness2, tWitness2, PktNumWitness3, PktNumSourceWitness3, EmpNumWitness3, EmpNameWitness3, EmpLicNumWitness3, tWitness3, PktNumWitness4, PktNumSourceWitness4, EmpNumWitness4, EmpNameWitness4, EmpLicNumWitness4, tWitness4, PktNumWitness5, PktNumSourceWitness5, EmpNumWitness5, EmpNameWitness5, EmpLicNumWitness5, tWitness5, EventComment, Asset, CardInEvtDisp, CardInEvtDesc, Source, tComplete)
+
+	DECLARE @UseAssetField char(255) = isnull((select case when Setting = 'Asset' then '1' else '0' end
+		                                         from RTSS.dbo.SYSTEMSETTINGS WITH (NOLOCK)
+												where ConfigSection = 'RTSSHH'
+												  and ConfigParam = 'EventLocationOrAssetFieldName'),'0')
+
+	insert into SQLA_MEAL (PktNum, Location, Zone, EmpNum, EmpName, EmpLicNum, tOut, EntryReason, ParentEventID,
+		                   PktNumWitness1, PktNumSourceWitness1, EmpNumWitness1, EmpNameWitness1, EmpLicNumWitness1,
+						   tWitness1, PktNumWitness2, PktNumSourceWitness2, EmpNumWitness2, EmpNameWitness2,
+						   EmpLicNumWitness2, tWitness2, PktNumWitness3, PktNumSourceWitness3, EmpNumWitness3,
+						   EmpNameWitness3, EmpLicNumWitness3, tWitness3, PktNumWitness4, PktNumSourceWitness4,
+						   EmpNumWitness4, EmpNameWitness4, EmpLicNumWitness4, tWitness4, PktNumWitness5,
+						   PktNumSourceWitness5, EmpNumWitness5, EmpNameWitness5, EmpLicNumWitness5, tWitness5,
+						   EventComment, Asset, CardInEvtDisp, CardInEvtDesc, Source, tComplete)
 	select ml.PktNum, ml.Location, ml.Zone,
-	       EmpNum = case when ml.EmpNum is null or ml.EmpNum = '' then ISNULL(ISNULL(ISNULL(es.EmpNumComplete,et.EmpNumComplete),es.EmpNumAuthorize),et.EmpNumAuthorize) else ml.EmpNum end,
-		   EmpName = case when ml.EmpName is null or ml.EmpName = '' then ISNULL(ISNULL(ISNULL(es.EmpNameComplete,et.EmpNameComplete),es.EmpNameAuthorize),et.EmpNameAuthorize) else ml.EmpName end,
+	       EmpNum = case when ml.EmpNum is null or ml.EmpNum = ''
+		                 then ISNULL(ISNULL(ISNULL(es.EmpNumComplete,et.EmpNumComplete),es.EmpNumAuthorize),et.EmpNumAuthorize)
+						 else ml.EmpNum
+						 end,
+		   EmpName = case when ml.EmpName is null or ml.EmpName = ''
+		                  then ISNULL(ISNULL(ISNULL(es.EmpNameComplete,et.EmpNameComplete),es.EmpNameAuthorize),et.EmpNameAuthorize)
+						  else ml.EmpName
+						  end,
 		   ml.EmpLicNum,
-		   ml.tOut, 
+		   ml.tOut,
 		   EntryReason = case when ml.EventDisplay1 = 'EMPCARD' then 'EMPCARD'
 		                      else isnull(ml.EntryReason, case when ml.EventDisplay1 = '' and ml.EventDisplay = '' then ml.EventComment
 		                                                       when ml.EventDisplay1 = '' and ml.EventDisplay <> '' then ml.EventDisplay
-		                                                       else ml.EventDisplay1 end) end, 
+		                                                       else ml.EventDisplay1 end) end,
 		   ParentEventID = case when ml.ParentEventID = 0 then ml.PktNumEvent else ml.ParentEventID end,
 		   ml.PktNumWitness1, ml.PktNumSourceWitness1, ml.EmpNumWitness1, ml.EmpNameWitness1, ml.EmpLicNumWitness1, ml.tWitness1,
 		   ml.PktNumWitness2, ml.PktNumSourceWitness2, ml.EmpNumWitness2, ml.EmpNameWitness2, ml.EmpLicNumWitness2, ml.tWitness2,
@@ -50,7 +65,10 @@ BEGIN
 		   EventComment = right(isnull(es.EventComment,et.EventComment),LEN(isnull(es.EventComment,et.EventComment))-CHARINDEX(';',isnull(es.EventComment,et.EventComment))),
 		   ml.Asset,
 		   CardInEvtDisp = isnull(isnull(isnull(isnull(isnull(crs.EventDisplay,crt.EventDisplay),rds.CmpReasonNum),rdt.CmpReasonNum),es.ResolutionDesc),et.ResolutionDesc),
-		   CardInEvtDesc = case when isnull(es.ResolutionDesc,et.ResolutionDesc) = 'Phantom' then ltrim(rtrim(isnull(isnull(isnull(isnull(crs2.EventDescription,crt2.EventDescription),rds2.CompleteReason),rdt2.CompleteReason),''))) + '; Phantom' else ltrim(rtrim(isnull(isnull(isnull(isnull(isnull(crs.EventDescription,crt.EventDescription),rds.CompleteReason),rdt.CompleteReason),es.ResolutionDesc),et.ResolutionDesc))) end,
+		   CardInEvtDesc = case when isnull(es.ResolutionDesc,et.ResolutionDesc) = 'Phantom'
+		                        then ltrim(rtrim(isnull(isnull(isnull(isnull(crs2.EventDescription,crt2.EventDescription),rds2.CompleteReason),rdt2.CompleteReason),''))) + '; Phantom'
+								else ltrim(rtrim(isnull(isnull(isnull(isnull(isnull(crs.EventDescription,crt.EventDescription),rds.CompleteReason),rdt.CompleteReason),es.ResolutionDesc),et.ResolutionDesc)))
+								end,
 		   Source = case when ml.PktCbMsg = 'RTA Offline' then 'RTA Offline'
 						 when et.PktNum is not null then 'TECH'
 						 else 'SLOT' end,
@@ -66,19 +84,45 @@ BEGIN
 	    on crs.EventDisplay = es.ResolutionDesc
 	  left join RTSS.dbo.CARDIN_REASON_ST as crt with (nolock)
 	    on crt.EventDisplay = et.ResolutionDesc
-	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting from RTSS.dbo.SYSTEMSETTINGS with (nolock) where ConfigSection = 'CompleteReason') as rds
+	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting
+	               from RTSS.dbo.SYSTEMSETTINGS with (nolock)
+				  where ConfigSection = 'CompleteReason'
+			    ) as rds
 	    on rds.CmpReasonNum = es.ResolutionDesc
-	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting from RTSS.dbo.SYSTEMSETTINGS with (nolock) where ConfigSection = 'CompleteReason_ST') as rdt
+	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting
+	               from RTSS.dbo.SYSTEMSETTINGS with (nolock)
+				  where ConfigSection = 'CompleteReason_ST'
+			    ) as rdt
 	    on rdt.CmpReasonNum = et.ResolutionDesc
 	  left join RTSS.dbo.CARDIN_REASON as crs2 with (nolock)
-	    on crs2.EventDisplay = left(es.EventComment,(case when CHARINDEX(';',es.EventComment) > 0 then CHARINDEX(';',es.EventComment)-1 else 0 end))
+	    on crs2.EventDisplay = left(es.EventComment,(case when CHARINDEX(';',es.EventComment) > 0
+		                                                  then CHARINDEX(';',es.EventComment)-1
+														  else 0
+														  end))
 	  left join RTSS.dbo.CARDIN_REASON_ST as crt2 with (nolock)
-	    on crt2.EventDisplay = left(et.EventComment,(case when CHARINDEX(';',et.EventComment) > 0 then CHARINDEX(';',et.EventComment)-1 else 0 end))
-	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting from RTSS.dbo.SYSTEMSETTINGS with (nolock) where ConfigSection = 'CompleteReason') as rds2
-	    on rds2.CmpReasonNum = left(es.EventComment,(case when CHARINDEX(';',es.EventComment) > 0 then CHARINDEX(';',es.EventComment)-1 else 0 end))
-	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting from RTSS.dbo.SYSTEMSETTINGS with (nolock) where ConfigSection = 'CompleteReason_ST') as rdt2
-	    on rdt2.CmpReasonNum = left(et.EventComment,(case when CHARINDEX(';',et.EventComment) > 0 then CHARINDEX(';',et.EventComment)-1 else 0 end))
-	 where not exists (select null from SQLA_MEAL as sm WITH (NOLOCK) where sm.PktNum = ml.PktNum)
+	    on crt2.EventDisplay = left(et.EventComment,(case when CHARINDEX(';',et.EventComment) > 0
+		                                                  then CHARINDEX(';',et.EventComment)-1
+														  else 0
+														  end))
+	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting
+	               from RTSS.dbo.SYSTEMSETTINGS with (nolock)
+				  where ConfigSection = 'CompleteReason'
+			    ) as rds2
+	    on rds2.CmpReasonNum = left(es.EventComment,(case when CHARINDEX(';',es.EventComment) > 0
+		                                                  then CHARINDEX(';',es.EventComment)-1
+														  else 0
+														  end))
+	  left join (select CmpReasonNum = cast(row_number() over(order by ConfigSection, ConfigParam)-1 as nvarchar), CompleteReason = Setting
+	               from RTSS.dbo.SYSTEMSETTINGS with (nolock)
+				  where ConfigSection = 'CompleteReason_ST'
+			    ) as rdt2
+	    on rdt2.CmpReasonNum = left(et.EventComment,(case when CHARINDEX(';',et.EventComment) > 0
+		                                                  then CHARINDEX(';',et.EventComment)-1
+														  else 0
+														  end))
+	 where not exists (select null
+		                 from SQLA_MEAL as sm WITH (NOLOCK)
+	                    where sm.PktNum = ml.PktNum)
 	   and (ml.tOut is not NULL and isdate(ml.tOut) = 1 and ml.tOut > '1/2/1980')
 	   and (    (ml.ParentEventID is null) or (ml.PktCbMsg = 'RTA Offline')
 	         or (ml.ParentEventID is not null and ml.ParentEventID = 0 and ml.PktNumEvent is null)
--- SQL/sp_SSRS_Rpt_RTA_MEAL.sql
+++ SQL/sp_SSRS_Rpt_RTA_MEAL.sql
@@ -10,7 +10,10 @@ GO
 
 
 
-IF EXISTS (select * from dbo.sysobjects where id = object_id(N'[dbo].[sp_SSRS_Rpt_RTA_MEAL]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+IF EXISTS (select *
+	         from dbo.sysobjects
+			where id = object_id(N'[dbo].[sp_SSRS_Rpt_RTA_MEAL]')
+			  and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 	DROP PROCEDURE [sp_SSRS_Rpt_RTA_MEAL]
 GO
 
@@ -21,41 +24,92 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE [dbo].[sp_SSRS_Rpt_RTA_MEAL] 
+CREATE PROCEDURE [dbo].[sp_SSRS_Rpt_RTA_MEAL]
 	@StartDt datetime,
 	@EndDt datetime,
 	@Location nvarchar(10) = ''
-	
+
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	
+
 	SELECT PktNum, Location, Zone, EmpNum,
-		   EmpName = case when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is not null then '('+left(emp.JobType,1)+') '+emp.NameFirst+' '+left(emp.NameLast,1)+'.'
-		                  when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is null and emp2.CardNum is not null then '('+left(emp2.JobType,1)+') '+emp2.NameFirst+' '+left(emp2.NameLast,1)+'.'
-						  else m.EmpName end,
-		   EmpLicNum, tOut, EntryReason, ParentEventID,
-		   PktNumWitness1, PktNumSourceWitness1, EmpNumWitness1, 
-		   EmpNameWitness1 = case when empw1.CardNum is null then EmpNameWitness1 else '('+left(empw1.JobType,1)+') '+empw1.NameFirst+' '+left(empw1.NameLast,1)+'.' end, 
-		   EmpLicNumWitness1, tWitness1,
-		   PktNumWitness2, PktNumSourceWitness2, EmpNumWitness2,
-		   EmpNameWitness2 = case when empw2.CardNum is null then EmpNameWitness2 else '('+left(empw2.JobType,1)+') '+empw2.NameFirst+' '+left(empw2.NameLast,1)+'.' end,
-		   EmpLicNumWitness2, tWitness2,
-		   PktNumWitness3, PktNumSourceWitness3, EmpNumWitness3,
-		   EmpNameWitness3 = case when empw3.CardNum is null then EmpNameWitness3 else '('+left(empw3.JobType,1)+') '+empw3.NameFirst+' '+left(empw3.NameLast,1)+'.' end,
-		   EmpLicNumWitness3, tWitness3,
-		   PktNumWitness4, PktNumSourceWitness4, EmpNumWitness4,
-		   EmpNameWitness4 = case when empw4.CardNum is null then EmpNameWitness4 else '('+left(empw4.JobType,1)+') '+empw4.NameFirst+' '+left(empw4.NameLast,1)+'.' end,
-		   EmpLicNumWitness4, tWitness4,
-		   PktNumWitness5, PktNumSourceWitness5, EmpNumWitness5,
-		   EmpNameWitness5 = case when empw5.CardNum is null then EmpNameWitness5 else '('+left(empw5.JobType,1)+') '+empw5.NameFirst+' '+left(empw5.NameLast,1)+'.' end,
-		   EmpLicNumWitness5, tWitness5,
-		   EventComment, Asset, 
-		   EventDescription = case when r.EventDescription is null and (m.CardInEvtDesc like '3C%' or m.CardInEvtDesc like '5A%') then ''
-								   when m.EntryReason = 'DROP TEAM' then ''
-		                           else isnull(r.EventDescription,rtrim(m.CardInEvtDesc)) end
+		   EmpName = case when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is not null
+		                  then '('+left(emp.JobType,1)+') '+emp.NameFirst+' '+left(emp.NameLast,1)+'.'
+		                  when (m.EmpName is null or ltrim(rtrim(m.EmpName)) = '') and emp.CardNum is null and emp2.CardNum is not null
+						  then '('+left(emp2.JobType,1)+') '+emp2.NameFirst+' '+left(emp2.NameLast,1)+'.'
+						  else m.EmpName
+						  end,
+		   EmpLicNum,
+		   tOut,
+		   EntryReason,
+		   ParentEventID,
+
+		   /***  Witness 1  ***/
+		   PktNumWitness1,
+		   PktNumSourceWitness1,
+		   EmpNumWitness1,
+		   EmpNameWitness1 = case when empw1.CardNum is null
+		                          then EmpNameWitness1
+								  else '('+left(empw1.JobType,1)+') '+empw1.NameFirst+' '+left(empw1.NameLast,1)+'.'
+								  end,
+		   EmpLicNumWitness1,
+		   tWitness1,
+
+		   /***  Witness 2  ***/
+		   PktNumWitness2,
+		   PktNumSourceWitness2,
+		   EmpNumWitness2,
+		   EmpNameWitness2 = case when empw2.CardNum is null
+		                          then EmpNameWitness2
+								  else '('+left(empw2.JobType,1)+') '+empw2.NameFirst+' '+left(empw2.NameLast,1)+'.'
+								  end,
+		   EmpLicNumWitness2,
+		   tWitness2,
+
+		   /***  Witness 3  ***/
+		   PktNumWitness3,
+		   PktNumSourceWitness3,
+		   EmpNumWitness3,
+		   EmpNameWitness3 = case when empw3.CardNum is null
+		                          then EmpNameWitness3
+								  else '('+left(empw3.JobType,1)+') '+empw3.NameFirst+' '+left(empw3.NameLast,1)+'.'
+								  end,
+		   EmpLicNumWitness3,
+		   tWitness3,
+
+		   /***  Witness 4  ***/
+		   PktNumWitness4,
+		   PktNumSourceWitness4,
+		   EmpNumWitness4,
+		   EmpNameWitness4 = case when empw4.CardNum is null
+		                          then EmpNameWitness4
+								  else '('+left(empw4.JobType,1)+') '+empw4.NameFirst+' '+left(empw4.NameLast,1)+'.'
+								  end,
+		   EmpLicNumWitness4,
+		   tWitness4,
+
+		   /***  Witness 5  ***/
+		   PktNumWitness5,
+		   PktNumSourceWitness5,
+		   EmpNumWitness5,
+		   EmpNameWitness5 = case when empw5.CardNum is null
+		                          then EmpNameWitness5
+								  else '('+left(empw5.JobType,1)+') '+empw5.NameFirst+' '+left(empw5.NameLast,1)+'.'
+								  end,
+		   EmpLicNumWitness5,
+		   tWitness5,
+
+		   EventComment = case when isnumeric(EventComment) = 1 then '' else EventComment end,
+		   Asset,
+		   EventDescription = case when r.EventDescription is null and (m.CardInEvtDesc like '3C%' or m.CardInEvtDesc like '5A%')
+		                           then ''
+								   when m.EntryReason = 'DROP TEAM'
+								   then ''
+		                           else isnull(r.EventDescription,rtrim(m.CardInEvtDesc))
+								   end
 	  FROM SQLA_MEAL as m
 	  left join SQLA_CardInReasons as r
 	    on r.Dept = m.Source
@@ -82,5 +136,3 @@ END
 
 
 GO
-
-
